b7b1a1a931c000f2f749b7d88585ae66211d76fd
Aniket Pandharabale <aniketicloud@gmail.com>
2023-05-06 16:37:00 +0530

    genercs, lists & adding gradient colors

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,12 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: 
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98)
+              ],
             ),
           ),
           child: const Center(
